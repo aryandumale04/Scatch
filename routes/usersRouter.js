5b23f85c2d6744e1a4c3c0b5cc7a9574801e963c
aryandumale04 <aryandumale04@gmail.com>
2025-10-07 23:01:18 +0530

    Add Routes

---
+++ routes/usersRouter.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function(req,res) {
+
+    res.send("Hey users route is working ");  
+
+})
+
+module.exports = router;
